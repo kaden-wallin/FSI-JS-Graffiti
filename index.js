7ceef5ba531922c1fd18db9e009594981f1e19f6
BuildTools <unconfigured@null.spigotmc.org>
2022-08-09 20:48:01 -0600

    Transfering code from VSCode to Replit

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+    header.style.textAlign = 'right'
+    header.style.backgroundColor = 'green'
+
+let dogNames = document.querySelectorAll('.dog-name')
+    for(let i = 0; i < dogeNames.length; i++){
+        dogNames[i].style.fontFamily = 'Comic sans MS, Trebuchet MS, Helvetica, sans-serif;'
+    }
+
+let dogImages = document.querySelectorAll('.dog-image')
+    dogImages[0].style.borderRadius = '50%'
+    dogImages[2].style.borderRadius = '50px'
+
+let footer = document.querySelector('.footer')
+    footer.style.borderStyle = 'solid'
+    footer.style.textAlign = 'left'
+    footer.style.backgroundColor = 'yellow'
+
+
